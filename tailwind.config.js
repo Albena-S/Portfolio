338bc2a9751ec6d55064bd07ae7f8218efacb3f9
Albena Stefanova <albena.stefanova@yahoo.com>
2023-04-10 16:53:36 +0200

    text color more visible

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.html', './**/**/*.js'],
+  content: ['./*.html', './**/**/*.js'],
   darkMode: 'media', // or 'class'
   theme: {
     extend: {},
